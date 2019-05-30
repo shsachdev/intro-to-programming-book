1a0acbcb39609645283cb1d23215dc1d49729393
Shikhar Sachdev <shsachdev@gmail.com>
2019-05-30 12:23:52 +0200

    finished array exercises

---
+++ arrays/ex1.rb
@@ -0,0 +1,8 @@
+array = [1, 3, 5, 7, 9, 11]
+number = 3
+
+array.each do |num|
+  if num == number
+    puts "#{number} is in the array."
+  end
+end 
---
+++ arrays/ex7.rb
@@ -0,0 +1,5 @@
+array = [1, 2, 3, 4, 5]
+
+array2 = array.map {|num| num + 2}
+p array
+p array2
